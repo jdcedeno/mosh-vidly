c2d81855f924667f1b5ebb42610b58d59c6bc92c
Jose Cedeno <56703919+jdcedeno@users.noreply.github.com>
2021-09-25 21:57:03 -0400

    add authentication jwt part 1

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
 
-const NavBar = () => {
+const NavBar = ({ user }) => {
     return (<nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container-fluid">
                     <Link className="navbar-brand" to="/">
@@ -27,16 +27,30 @@ const NavBar = () => {
                                     Rentals
                                 </NavLink>
                             </li>
-                            <li className="nav-item">
-                                <NavLink className="nav-link" to="/login">
-                                    Login
-                                </NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink className="nav-link" to="/register">
-                                    Register
-                                </NavLink>
-                            </li>
+                            {!user && <React.Fragment>
+                                <li className="nav-item">
+                                    <NavLink className="nav-link" to="/login">
+                                        Login
+                                    </NavLink>
+                                </li>
+                                <li className="nav-item">
+                                    <NavLink className="nav-link" to="/register">
+                                        Register
+                                    </NavLink>
+                                </li>
+                            </React.Fragment>}
+                            {user && <React.Fragment>
+                                <li className="nav-item">
+                                    <NavLink className="nav-link" to="/profile">
+                                        {user.name}
+                                    </NavLink>
+                                </li>
+                                <li className="nav-item">
+                                    <NavLink className="nav-link" to="/logout">
+                                        Logout
+                                    </NavLink>
+                                </li>
+                            </React.Fragment>}
                         </ul>
                     </div>
                 </div>
